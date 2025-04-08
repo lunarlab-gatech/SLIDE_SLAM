f41ddc570d66b20f75b0374e1b35fa5986aa19d5
cjackson357 <cjackson357@gatech.edu>
2025-04-07 20:06:24 -0400

    github actions test 4/7 20:06

--- docker/run_container.sh
+++ docker/run_container.sh
@@ -9,6 +9,16 @@ if [ -z "$CI" ]; then
   DOCKER_RUN_FLAGS="-it"
 fi
 
+EXTRA_VOLUMES=""
+if [ -z "$CI" ]; then
+  EXTRA_VOLUMES+=" --env=\"DISPLAY=$DISPLAY\""
+  EXTRA_VOLUMES+=" --env=\"QT_X11_NO_MITSHM=1\""
+  EXTRA_VOLUMES+=" --env=\"XAUTHORITY=/root/.Xauthority\""
+  EXTRA_VOLUMES+=" --volume=\"/tmp/.X11-unix:/tmp/.X11-unix:rw\""
+  EXTRA_VOLUMES+=" --volume=\"/home/$USER/.bash_aliases:/root/.bash_aliases\""
+  EXTRA_VOLUMES+=" --volume=\"$XAUTHORITY:/root/.Xauthority:ro\""
+fi
+
 docker run $DOCKER_RUN_FLAGS \
     --name="slideslam_ros" \
     --net="host" \
@@ -16,17 +26,11 @@ docker run $DOCKER_RUN_FLAGS \
     --gpus="all" \
     --workdir="/opt/slideslam_docker_ws" \
     --env="DISPLAY=$DISPLAY" \
-    --env="QT_X11_NO_MITSHM=1" \
-    --env="XAUTHORITY=/root/.Xauthority" \
     --volume="$SlideSlamWs:/opt/slideslam_docker_ws" \
     --volume="$SlideSlamCodeDir:$SlideSlamCodeDir" \
     --volume="$BAGS_DIR:/opt/bags" \
-    --volume="/home/$USER/.bash_aliases:/root/.bash_aliases" \
-    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     --volume="/home/$USER/repos:/home/$USER/repos" \
-    if [ -n "$XAUTHORITY" ]; then
-    DOCKER_ARGS="$DOCKER_ARGS -v $XAUTHORITY:/root/.Xauthority:ro"
-    fi
+    $EXTRA_VOLUMES \
     slideslam_ros_image \
     bash
 
