31a293259db887ea19bf7ca1b70a31c13f15d909
Daniel Butterfield <danielcbutter@gmail.com>
2025-11-25 18:43:15 +0000

    Minor changes

--- docker/run_container.sh
+++ docker/run_container.sh
@@ -1,5 +1,5 @@
-REPO_DIR="/mnt/c/Users/camer/Documents/LunarLab/slide_slam_mk2"
-DATA_DIR="/mnt/d/Hercules/V1.4.1/data/bags"
+WS_DIR="/home/$USER/Research/ros_workspaces/slideslam_original_ws"
+DATA_DIR="/home/$USER/Desktop/data/"
 
 export XAUTHORITY=$HOME/.Xauthority
 
@@ -14,7 +14,7 @@ docker run -it \
     --env="XAUTHORITY=/tmp/.Xauthority" \
     --env="USER_ID=$(id -u)" \
     --env="GROUP_ID=$(id -g)" \
-    --volume="$REPO_DIR:/home/$USER/slideslam_original_ws" \
+    --volume="$WS_DIR:/home/$USER/slideslam_original_ws" \
     --volume="$DATA_DIR:/home/$USER/data" \
     --volume="/home/$USER/.bash_aliases:/home/$USER/.bash_aliases" \
     --volume="/home/$USER/.ssh:/home/$USER/.ssh:ro" \
